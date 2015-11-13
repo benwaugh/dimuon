e100c67aeea0753f70dec104d641333128c5074a
Ben Waugh <b.waugh@ucl.ac.uk>
2015-11-13 12:17:04 +0000

    Loop over events and print number of particles

--- dimuon.py
+++ dimuon.py
@@ -4,3 +4,8 @@ f = TFile("test_data/events.root")
 t = f.Get("events")
 n = t.GetEntries()
 print "Number of events = " + str(n)
+
+for i in xrange(n):
+    t.GetEntry(i)
+    n2 = t.nPart
+    print "Number of particles = " + str(n2)
