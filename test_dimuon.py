7e19c3058615f4599ed7339e2bd157b72cd51018
Ben Waugh <b.waugh@ucl.ac.uk>
2015-11-13 21:09:07 +0000

    Test pair mass for non-zero mass particles

--- test_dimuon.py
+++ test_dimuon.py
@@ -35,4 +35,10 @@ def test_inv_mass_zero_mass_particles():
     pos = Particle(1.0, +1.0,  0, pi/2) # massless particle with pt = 1 GeV
     neg = Particle(1.0, -1.0, pi, pi/2) # massless, pt = 1 GeV, opposite direction
     assert_equal(inv_mass_from_pair((pos,neg)), 2.0)
-    
+
+def test_inv_mass_nonzero_mass_particles():
+    # shouldn't actually make any difference if masses are non-zero
+    pos = Particle(1.0, +0.5,  0, pi/2)
+    neg = Particle(1.0, -0.5, pi, pi/2)
+    assert_equal(inv_mass_from_pair((pos,neg)), 2.0)
+
